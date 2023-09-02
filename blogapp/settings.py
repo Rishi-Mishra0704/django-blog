db367902036319645757fad84d5aa8e3986002d7
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-02 21:14:31 +0530

    add postgres settings

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'blogapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'blog_app',
+       'USER': 'rishi',
+       'PASSWORD': '1111',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
