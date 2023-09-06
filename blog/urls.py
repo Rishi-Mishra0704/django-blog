6559f583418ab9ef0b871e3dfd04945596349474
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-06 21:49:53 +0530

    Add read later page

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts/", views.allPosts, name="posts-page"),
     path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later-page"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -36,7 +36,7 @@ class SinglePostView(DetailView):
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
-            comment.post = post 
+            comment.post = post
             comment.save()
             return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
 
@@ -46,3 +46,36 @@ class SinglePostView(DetailView):
             "comment_form": CommentForm(),
         }
         return render(request, "blog/post-details.html", context)
+
+
+class ReadLaterView(View):
+
+    def get(self, request):
+        stored_post = request.session.get("stored_post")
+
+        context = {}
+
+        if stored_post is None or len(stored_post) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_post)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+
+    def post(self, request):
+        stored_post = request.session.get("stored_post")
+
+        if stored_post is None:
+            stored_post = []
+
+        post_id = int(request.POST["post_id"])
+
+        if post_id not in stored_post:
+            stored_post.append(post_id)
+            request.session["stored_post"] = stored_post
+
+        return HttpResponseRedirect("/")
