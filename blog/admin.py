3d457d257072a8b9848b4bf73b4742a4242cf74f
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-09-06 20:52:29 +0530

    Add comment to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Tag, Post
+from .models import Author, Tag, Post, Comment
 
 # Register your models here.
 
@@ -10,6 +10,13 @@ class postAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+
+class commentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+    list_filter = ("user_name", "post", "user_email")
+
+
 admin.site.register(Post, postAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, commentAdmin)
